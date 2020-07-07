72a22112f84dddca7951e27a1f85ede6bb0f2976
Mushtak Hussain <34830693+Mu3htak@users.noreply.github.com>
2020-07-07 14:16:06 +0530

    Update To_do.py

--- To_do.py
+++ To_do.py
@@ -2,6 +2,7 @@
 import shelve
 import sys
 import datetime
+import argparse
 
 fileshow = shelve.open("mythings", writeback=True)
 list_to_do = []  
@@ -9,51 +10,57 @@ update = ''
 date = datetime.date.today()
 date = date.strftime("%B-%d-%Y -> ")
 
-try:
-    if sys.argv[1] == 'update':
-        print("Add To-Do List, 'Type exit to stop'")
-        while True:
-            update = input()
-            if update == 'exit':
-                break
-            update = date + update
-            fileshow['list_to_do'].append(update)
+parser = argparse.ArgumentParser(description="Add To-Do List")
+parser.add_argument('-s','--show',action='store_true',help="Displays the task To-Do")
+parser.add_argument('-u','--update',action='store_true', help="Update the list To-Do in your current task")
+parser.add_argument('-n','--new',action='store_true', help="Re-Creates a new list To-Do")
+parser.add_argument('-r','--remove',action='store_true', help="Remove the specific task in list To-Do")
+args = parser.parse_args()
+
+if len(sys.argv) < 2:
+    print("Usage:python To_do.py [-h]")
+
+if args.update:
+    print("Add To-Do List, 'Type exit to stop'")
+    while True:
+        update = input()
+        if update == 'exit':
+            break
+        update = date + update
+        fileshow['list_to_do'].append(update)
     
-    elif sys.argv[1] == 'show':
-        print('')
-        print("No   Registered \t Things To Do")
+elif args.show:
+    print('')
+    print("No   Registered \t Things To Do")
+    print(" ".rjust(50,'-'))
+    for i in range(len(fileshow['list_to_do'])):
+        print("%d - %s" % (i,fileshow['list_to_do'][i]))
         print(" ".rjust(50,'-'))
-        for i in range(len(fileshow['list_to_do'])):
-            print("%d - %s" % (i,fileshow['list_to_do'][i]))
-            print(" ".rjust(50,'-'))
-        sys.exit()        
+    sys.exit()        
     
-    elif sys.argv[1] == 'remove':
-        print("Enter Item Number To Remove The From List")
-        rem = int(input())
-        fileshow['list_to_do'].pop(rem)
-        print("Removed..")
-        sys.exit()
+elif args.remove:
+    print("Enter Item Number To Remove The From List")
+    rem = int(input())
+    fileshow['list_to_do'].pop(rem)
+    print("Removed..")
+    sys.exit()
     
-    elif sys.argv[1] == 'new':
-        print("Welcome To-Do!, Mushtak :)")
-        print("Add To-Do items.")
-        print("If you're done adding type 'exit'")
+elif args.new:
+    print("Welcome To-Do!)")
+    print("Add To-Do items.")
+    print("If you're done adding type 'exit'")
     
-        while True:
-            add = input()
-            if add == 'exit':
-                break
-            add = date + add
-            list_to_do += [add]
-            fileshow['list_to_do'] = list_to_do
+    while True:
+        add = input()
+        if add == 'exit':
+            break
+        add = date + add
+        list_to_do += [add]
+        fileshow['list_to_do'] = list_to_do
     else:
         print("Invalid argument!")
         print("Usage: python To_do.py [show] [update] [new] [remove]")
     fileshow.sync()
     fileshow.close()
-        
-except IndexError:
-    if len(sys.argv) < 2:
-        print("Usage: python To_do.py [show] [update] [new] [remove]")
+
       
